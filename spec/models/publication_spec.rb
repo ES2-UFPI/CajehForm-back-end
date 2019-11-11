b7ecf7b0068542cf52591c546d001c58d53839f2
Eduardo <jeduardo824@gmail.com>
2019-11-11 12:39:27 -0300

    add specs and validation to Publication model #10

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Publication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with valid attributes" do
+    subject do
+      described_class.new(
+        title: "Testing a publication",
+        content: "Content to test a Publication. Lets see if this works.",
+        user: create(:user)
+      )
+    end
+    it "is valid" do
+      expect(subject).to be_valid
+    end
+  end
+
+  context "without title" do
+    subject do
+      described_class.new(
+        title: "Testing a publication",
+        user: create(:user)
+      )
+    end
+    it "is invalid" do
+      expect(subject).not_to be_valid
+    end
+  end
+
+  context "without content" do
+    subject do
+      described_class.new(
+        content: "Content to test a Publication. Lets see if this works.",
+        user: create(:user)
+      )
+    end
+    it "is invalid" do
+      expect(subject).not_to be_valid
+    end
+  end
+
+  context "without user" do
+    subject do
+      described_class.new(
+        title: "Testing a publication",
+        content: "Content to test a Publication. Lets see if this works."
+      )
+    end
+    it "is invalid" do
+      expect(subject).not_to be_valid
+    end
+  end
 end
